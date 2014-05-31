3b4153ac50e0819a256fc89334cfc9973ff66b6f
Craig Buchek <craig@boochtek.com>
2014-05-31 10:11:50 -0500

    Mapper: Implement #select.

--- lib/preserves/mapper.rb
+++ lib/preserves/mapper.rb
@@ -4,5 +4,23 @@ module Preserves
       pg_result = SQL.connection(dbname: "preserves_test").exec(sql_string)
       SQL::Result.new(pg_result)
     end
+
+    def select(sql_string)
+      pg_result = SQL.connection(dbname: "preserves_test").exec(sql_string)
+      fields = pg_result.fields
+      (0..pg_result.ntuples-1).map{|n| hash_to_object(pg_result[n])}
+    end
+
+  private
+
+    def hash_to_object(hash)
+      object = User.new
+      hash.each_pair do |column_name, field_value|
+        if object.respond_to?("#{column_name}=")
+          object.send("#{column_name}=", field_value)
+        end
+      end
+      object
+    end
   end
 end
--- spec/mapper_spec.rb
+++ spec/mapper_spec.rb
@@ -2,6 +2,11 @@ require "spec_helper"
 require "preserves"
 
 
+class User
+  attr_accessor :id
+end
+
+
 describe "Mapper" do
   subject(:mapper) { Preserves::Mapper.new }
 
@@ -20,6 +25,25 @@ describe "Mapper" do
 
     it "returns the number of rows processed" do
       expect(query.rows).to eq(1)
+    end
+  end
+
+  describe "selecting results from a query to an object" do
+    describe "when DB has one user" do
+      before do
+        mapper.query("INSERT INTO users (username, name, age) VALUES ('booch', 'Craig', 43)")
+      end
+
+      let(:selection) { mapper.select("SELECT username AS id FROM users") }
+
+      it "returns a set of 1 User object" do
+        expect(selection.size).to eq(1)
+        expect(selection.first.class).to eq(User)
+      end
+
+      it "sets the attributes on the object" do
+        expect(selection.first.id).to eq("booch")
+      end
     end
   end
 end
